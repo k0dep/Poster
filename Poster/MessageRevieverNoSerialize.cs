be4015ccabf2c13a2a5e2592c3e651dbd8197dee
Petrov Valeriovich Stanislav <ctapbiu_kodep@hotmail.com>
2018-07-06 23:26:22 +0300

    MessageSender и Reciever без серилизации

---
+++ Poster/MessageRevieverNoSerialize.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Poster
+{
+    public class MessageRevieverNoSerialize : IMessageReceiver, IMessageRecieverHistory
+    {
+        public IMessageBindAccess MessageBindAccess { get; private set; }
+        public IList<MessageLogItem> MessageHistory { get; private set; }
+        
+        public bool ThrowNoneBinds { get; set; }
+
+        public MessageRevieverNoSerialize(IMessageBindAccess bindAccess, int historyLen = 100)
+        {
+            MessageBindAccess = bindAccess;
+            MessageHistory = new CycleList<MessageLogItem>(historyLen);
+        }
+
+
+        public void Receive(string name, byte[] message, object custom)
+        {
+            var logItem = new MessageLogItem(DateTime.UtcNow, name, message, custom);
+            MessageHistory.Add(logItem);
+
+            if (!MessageBindAccess.AnyBind(name))
+            {
+                if (ThrowNoneBinds)
+                {
+                    throw new MessageNotBindedException("Received not binded message: " + name, name, message);
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            var binds = MessageBindAccess.GetBinds(name);
+
+            for (int i = 0; i < binds.Count; i++)
+            {
+                binds[i].BindAction(custom);
+            }
+        }
+    }
+}
---
+++ Poster/MessageSenderNoSerialize.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Poster
+{
+    public class MessageSenderNoSerialize : IMessageSender
+    {
+        private readonly IMessageReceiver _receiver;
+
+        public MessageSenderNoSerialize(ISerializationProvider serializator, IMessageReceiver reciever)
+        {
+            _receiver = reciever;
+        }
+
+        public void Send(string name, object message, Action<MessageSendError> sendError)
+        {
+            _receiver.Receive(name, null, message);
+        }
+
+        public void Send(string name, object message)
+        {
+            Send(name, message, error => { });
+        }
+
+
+        public void Send<TMessage>(TMessage message)
+        {
+            Send(message, error => { });
+        }
+
+
+        public void Send<TMessage>(TMessage message, Action<MessageSendError> sendError)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException("message");
+            }
+
+            var messageType = typeof(TMessage);
+            var name = messageType.FullName;
+
+            var msgAttr = messageType.GetCustomAttributes(typeof(MessageAttribute), false);
+            if (msgAttr.Length != 0)
+                name = ((MessageAttribute) msgAttr[0]).MessageName;
+
+            Send(name, message, sendError);
+        }
+    }
+}
